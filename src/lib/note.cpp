602da2df3e248e7f047a625c7187b9544761b23d
Joshua Rubin <jrubin@esoft.com>
2010-07-26 12:52:30 -0600

    add an initial note class

--- src/lib/note.cpp
+++ src/lib/note.cpp
@@ -1,3 +1,79 @@
 // Copyright 2010 Rubix Consulting, Inc.
 
+#include "./note.h"
+#include <string>
+#include <vector>
+
+namespace rubix {
+note::note() {
+  _init();
+}
+
+note::note(const int& idval, const std::string& val,
+           const boost::gregorian::date& create,
+           const boost::gregorian::date& modify) {
+  _init();
+  id(idval);
+  value(val);
+  _created(create);
+  _modified(modify);
+}
+
+void note::_init() {
+  id(0);
+  value("");
+  _created(boost::gregorian::date());
+  _modified(boost::gregorian::date());
+  __tags.clear();
+}
+
+void note::clear() {
+  _init();
+}
+
+const uint32_t& note::id() {
+  return __id;
+}
+
+const uint32_t& note::id(const uint32_t& val) {
+  __id = val;
+  return id();
+}
+
+const std::string& note::value() {
+  return __value;
+}
+
+const std::string& note::value(const std::string& val) {
+  __value = val;
+  // TODO(jrubin) parse tags
+  _modified(boost::gregorian::date());
+  return value();
+}
+
+const boost::gregorian::date& note::created() {
+  return __created;
+}
+
+const boost::gregorian::date& note::_created(
+    const boost::gregorian::date& val) {
+  __created = val;
+  return created();
+}
+
+const boost::gregorian::date& note::modified() {
+  return __modified;
+}
+
+const boost::gregorian::date& note::_modified(
+    const boost::gregorian::date& val) {
+  __modified = val;
+  return modified();
+}
+
+const std::vector<std::string>& note::tags() {
+  return __tags;
+}
+};
+
 // vim: textwidth=80:wrap:expandtab:tabstop=2:formatoptions=croqlt:shiftwidth=2
--- src/lib/note.h
+++ src/lib/note.h
@@ -3,6 +3,43 @@
 #ifndef SRC_LIB_NOTE_H_
 #define SRC_LIB_NOTE_H_
 
+#include <boost/date_time/gregorian/gregorian.hpp>
+#include <string>
+#include <vector>
+
+namespace rubix {
+class note {
+  public:
+    // constructors
+    note();
+    note(const int&, const std::string&, const boost::gregorian::date&,
+         const boost::gregorian::date&);
+
+    // methods
+    void                            clear();
+    const uint32_t&                 id();
+    const uint32_t&                 id(const uint32_t&);
+    const std::string&              value();
+    const std::string&              value(const std::string&);
+    const boost::gregorian::date&   created();
+    const boost::gregorian::date&   modified();
+    const std::vector<std::string>& tags();
+
+  private:
+    // properties
+    uint32_t                 __id;
+    std::string              __value;
+    boost::gregorian::date   __created;
+    boost::gregorian::date   __modified;
+    std::vector<std::string> __tags;
+
+    // methods
+    void                          _init();
+    const boost::gregorian::date& _created(const boost::gregorian::date&);
+    const boost::gregorian::date& _modified(const boost::gregorian::date&);
+};
+};
+
 #endif  // SRC_LIB_NOTE_H_
 
 // vim: textwidth=80:wrap:expandtab:tabstop=2:formatoptions=croqlt:shiftwidth=2
